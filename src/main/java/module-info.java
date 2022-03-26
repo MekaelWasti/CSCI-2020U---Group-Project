644d601e22b565c7ce0e593766b2a5a660da4fb0
Mekael Wasti <mekael_wasti@hotmail.com>
2022-03-26 05:32:32 -0400

    Set up Java Fx project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module final_project.csci2020u_final_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires javafx.graphics;
+
+    opens final_project.csci2020u_final_project to javafx.fxml;
+    exports final_project.csci2020u_final_project;
+}
